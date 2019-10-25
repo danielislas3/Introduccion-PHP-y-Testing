12eb1138eed9c4eac50ecbbf2d69c496fad1f4a2
Daniel Orio <danielislas3@live.com>
2019-10-25 13:24:24 -0500

    Testing init

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
 <?php
   $var1=1;
-  $name='Daniel Orio';
+  $name='Name';
+  $array=[
+    0=>'index1',
+    1=>'index2',
+    2=>'index3',
+    4=>'index4',
+  ];
+  var_dump($array[0]);
+  
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en">  
 
 <head>
   <meta charset="UTF-8">
@@ -15,7 +23,10 @@
 
 <body>
   <h1>Hola</h1>
+  <P>VARIABLES</P>
   <h1><?php echo $name;?> </h1>
+  <P>ARRAY</P>
+  <h2><?php echo $array[3];?> </h2>
 </body>
 
 </html>
