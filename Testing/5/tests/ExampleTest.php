76813839c60a7e07da720525bf12d9fec7941fa0
Daniel Orio <danielislas3@live.com>
2019-10-28 13:23:47 -0600

    6- Aserciones o afirmaciones

---
+++ Testing/5/tests/ExampleTest.php
@@ -0,0 +1,51 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase{
+
+  public function testTrue(){
+    $this->assertTrue(true);
+  } 
+  public function testFalse(){
+    $var= false;
+    $this->assertFalse($var);
+  } 
+  public function testEquals(){
+    $result= 5 + 5;
+    $this->assertEquals($result, "10");
+  } 
+  public function testSame(){
+    $result= 5 + 5;
+    $this->assertSame($result, 10);
+  } 
+  public function testArray(){
+    $result= [];
+    $this->assertIsArray($result);
+  } 
+  public function testBool(){
+    $result= false;
+    $this->assertIsBool($result);
+  } 
+  public function testString(){
+    $result= "false";
+    $this->assertIsString($result);
+  } 
+  public function testEmpty(){
+    $result= [];
+    $this->assertEmpty($result);
+  } 
+  public function testCount(){
+    $arr=['Amarillo','Azul'];
+    $this->assertCount(2,$arr);
+  } 
+  public function testHasKey(){
+    $arr=['color'=>'Azul'];
+    $this->assertArrayHasKey('color',$arr);
+  } 
+  /***
+  *
+  * Una afirmacion analiza el resultado de una clase
+  *
+  **/
+}
