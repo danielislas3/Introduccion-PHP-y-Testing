89c95351b4a93b85ad6960f571e4d2fb7aa5a7c0
Daniel Orio <danielislas3@live.com>
2019-10-31 16:32:50 -0600

    Font Controller

--- Platzi/curso-introduccion-php-7-functions/addJob.php
+++ Platzi/curso-introduccion-php-7-functions/addJob.php
@@ -1,28 +1,9 @@
 <?php
 // var_dump($_GET);
 // var_dump($_POST);
-require_once 'vendor/autoload.php';
 
-use Illuminate\Database\Capsule\Manager as Capsule;
 use App\Models\{Job, Proyect};
 
-$capsule = new Capsule;
-
-$capsule->addConnection([
-  'driver'    => 'mysql',
-  'host'      => 'localhost',
-  'database'  => 'cursophp',
-  'username'  => 'root',
-  'password'  => '',
-  'charset'   => 'utf8',
-  'collation' => 'utf8_unicode_ci',
-  'prefix'    => '',
-]);
-// Make this Capsule instance available globally via static methods... (optional)
-$capsule->setAsGlobal();
-
-// Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
-$capsule->bootEloquent();
 
 if (!empty($_POST)) {
 
