3f311393015ff66cf7f33253a26ddf1e8fc27cb7
Daniel Orio <danielislas3@live.com>
2019-11-06 11:48:19 -0600

    vadidador

--- Platzi/curso-introduccion-php-7-functions/app/Controllers/JobsController.php
+++ Platzi/curso-introduccion-php-7-functions/app/Controllers/JobsController.php
@@ -2,28 +2,41 @@
 
 namespace App\Controllers;
 use App\Models\{Job, Proyect};
+use Respect\Validation\Validator as v;
 
 
 class JobsController extends BaseController
 {
   public function getAddJoobAction($request)
   {
-
+    $responseMessage='';
     if ($request->getMethod() == 'POST') {
       $postData = $request->getParsedBody();
+      $jobValidator = v::key('title', v::stringType()->notEmpty())
+                  ->key('description', v::stringType()->notEmpty());
 
-      $job = new Job();
-      $proyect = new Proyect();
-
-      $proyect->title = $postData['titleProyect'];
-      $proyect->description = $postData['descriptionProyect'];
-
-      $job->title = $postData['title'];
-      $job->description = $postData['description'];
-
-      $job->save();
-      $proyect->save();
+          try{
+            $jobValidator->assert($postData); // trueF
+            $job = new Job();
+            $proyect = new Proyect();
+      
+            $proyect->title = $postData['titleProyect'];
+            $proyect->description = $postData['descriptionProyect'];
+      
+            $job->title = $postData['title'];
+            $job->description = $postData['description'];
+      
+            $job->save();
+            $proyect->save();
+            $responseMessage='Saved';
+          } catch(\Exception $e){
+            var_dump($e->getMessage());
+            $responseMessage=$e->getMessage();
+          }       
+              
     }
-    return ($this->renderHTML('addJob.twig'));
+    return ($this->renderHTML('addJob.twig',[
+      'responseMessage'=>$responseMessage
+    ]));
   }
 }
